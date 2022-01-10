5ff86e7d183b8902c5ec4d6940f81337b1cff3e9
김영수 <iloveu0920@gmail.com>
2022-01-10 18:03:46 +0900

    예제 추가

--- Exercise01/Program.cs
+++ Exercise01/Program.cs
@@ -6,7 +6,25 @@ namespace Exercise01
     {
         static void Main(string[] args)
         {
+            int a = 20;
+            int b = 020;
+            int c = 0x20;
+
+            const int TRAINING = 0x0001;        //bit 00000001
+            const int FULLSCREEN = 0x0002;      //bit 00000010
+            const int FLAG1 = 0x0004;           //bit 00000100
+            const int FLAG2 = 0x0008;           //bit 00001000
+            const int FLAG3 = 0x0010;           //bit 00010000
+            const int FLAG4 = 0x0020;           //bit 00100000
+
+            int mode = TRAINING | FULLSCREEN | FLAG3;
+            int temp = mode & FULLSCREEN;
+
             Console.WriteLine("Hello World!");
+            Console.WriteLine(String.Format("{0}, {1}, {2}", a, b, c));
+
+            if(temp != 0)
+            {}
         }
     }
 }
---
+++ Generictest/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Generictest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList a = new ArrayList();
+            List<int> b = new List<int>();
+            List<string> c = new List<string>();
+
+            a.Add(4);
+            a.Add("test");
+
+            b.Add(4);
+            c.Add("test");
+
+            foreach (Object i in a)
+            {
+                Console.WriteLine(i.ToString());
+            }
+
+            foreach (Object i in b)
+            {
+                Console.WriteLine(i.ToString());
+            }
+        }
+    }
+}
