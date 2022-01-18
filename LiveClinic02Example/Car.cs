aaa605221917d65493fd4196df4d416a63442a9f
PiaMentor <sj.lee@pia.space>
2022-01-18 14:13:20 +0900

    라이브클리닉 #2 강의내용

---
+++ LiveClinic02Example/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace LiveClinic02Example
+{
+    class Car
+    {
+        public void Run()
+        {
+            Console.WriteLine("자동차가 달립니다.");
+        }
+
+        public virtual void Refuel()
+        {
+            Console.WriteLine("급유합니다.");
+        }
+    }
+
+
+    class ElectricCar : Car
+    {
+        public new void Run()
+        {
+            Console.WriteLine("전기차가 달립니다.");
+        }
+
+        public override void Refuel()
+        {
+            Console.WriteLine("충전합니다.");
+        }
+    }
+
+}
---
+++ LiveClinic02Example/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace LiveClinic02Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Car carA = new Car();
+            Car carB = new ElectricCar();
+            ElectricCar carC = new ElectricCar();
+
+            carA.Run();
+            carB.Run();
+            carC.Run();
+
+            Console.WriteLine();
+
+            carA.Refuel();
+            carB.Refuel();
+            carC.Refuel();
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+
+            List<Car> carList = new List<Car>();
+
+            carList.Add(new Car());
+            carList.Add(new ElectricCar());
+            carList.Add(new Car());
+
+            foreach (Car car in carList)
+            {
+                car.Run();
+                car.Refuel();
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
