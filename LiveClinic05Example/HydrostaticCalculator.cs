07cbfe0f0ccdd70496618c6dd1739849ce29a6a5
PiaMentor <sj.lee@pia.space>
2022-02-14 19:02:48 +0900

    라이브클리닉 #5 강의코드 추가

---
+++ LiveClinic05Example/HydrostaticCalculator.cs
@@ -0,0 +1,153 @@
+﻿using System;
+
+
+namespace LiveClinic05Example
+{
+    class HydrostaticCalculator
+    {
+        // offset table과 station 간격을 저장할 멤버 변수 (둘 다 meter 단위)
+        protected double[,] offset_m;
+        protected double ds_m;
+        protected double dw_m;
+
+
+        // 생성자 (m 단위로 입력되는 경우)
+        public HydrostaticCalculator(double[,] offset_m, double ds_m, double dw_m)
+        {
+            this.offset_m = offset_m;
+            this.ds_m = ds_m;
+            this.dw_m = dw_m;
+        }
+
+        // 생성자 (mm 단위로 입력되는 경우)
+        public HydrostaticCalculator(int[,] offset_mm, double ds_m, double dw_m)
+            : this((double[,])null, ds_m, dw_m)
+        {
+            int m = offset_mm.GetLength(0);
+            int n = offset_mm.GetLength(1);
+
+            this.offset_m = new double[m, n];
+            for (int i = 0, j; i < m; i++)
+                for (j = 0; j < n; j++)
+                    this.offset_m[i, j] = (double)offset_mm[i, j] * 0.001;
+        }
+
+        // 전체 waterline area (수선면적)을 모두 계산하는 함수
+        public double[] Calculate_waterline_areas()
+        {
+            int n = this.offset_m.GetLength(1);   // number of waterlines
+
+            double[] areas = new double[n];
+
+            for (int w = 0; w < n; w++)
+            {
+                areas[w] = Calculate_waterline_area(w);
+            }
+
+            return areas;
+        }
+
+        // 하나의 waterline area를 계산하는 함수 (w 위치)
+        protected virtual double Calculate_waterline_area(int w)
+        {
+            double area = 0.0;
+
+            int m = this.offset_m.GetLength(0);   // number of stations
+
+            for (int s = 0; s < m - 1; s++)
+            {
+                area += 2.0 * Trapezoidal(s, w, axis: 0);
+            }
+
+            return area;
+        }
+
+        // 사다리꼴 공식으로 면적 계산
+        //  나중에 함수를 인자로 전달하여 일반화 해보자 (모멘트 계산 등에 사용할 수 있도록)
+        protected double Trapezoidal(int s_pos, int w_pos, int axis=0)
+        {
+            double x, y1, y2;
+            if (axis == 0)
+            {
+                x = this.ds_m;
+                y1 = this.offset_m[s_pos, w_pos];
+                y2 = this.offset_m[s_pos + 1, w_pos];
+            }
+            else if (axis == 1)
+            {
+                x = this.dw_m;
+                y1 = this.offset_m[s_pos, w_pos];
+                y2 = this.offset_m[s_pos, w_pos + 1];
+            }
+            else
+            {
+                throw new Exception("axis가 범위를 벗어났습니다.");
+            }
+
+            return 0.5 * x * (y1 + y2);
+        }
+
+    }
+
+
+
+    class HydrostaticCalculator_Simpson1 : HydrostaticCalculator
+    {
+
+        public HydrostaticCalculator_Simpson1(double[,] offset_m, double ds_m, double dw_m)
+            : base(offset_m, ds_m, dw_m)
+        {
+        }
+
+        public HydrostaticCalculator_Simpson1(int[,] offset_mm, double ds_m, double dw_m)
+            : base(offset_mm, ds_m, dw_m)
+        {
+        }
+
+        protected override double Calculate_waterline_area(int w)
+        {
+            double area = 0.0;
+
+            int m = this.offset_m.GetLength(0);   // number of stations
+
+            int s;
+            for (s = 0; s < m - 2; s += 2)
+            {
+                area += 2.0 * Simpson1(s, w, axis: 0);
+            }
+
+            if (s < m - 1)
+            {
+                area += 2.0 * Trapezoidal(s, w, axis: 0);
+            }
+            return area;
+        }
+
+        protected double Simpson1(int s_pos, int w_pos, int axis = 0)
+        {
+            double x, y1, y2, y3;
+            if (axis == 0)
+            {
+                x = this.ds_m;
+                y1 = this.offset_m[s_pos, w_pos];
+                y2 = this.offset_m[s_pos + 1, w_pos];
+                y3 = this.offset_m[s_pos + 2, w_pos];
+            }
+            else if (axis == 1)
+            {
+                x = this.dw_m;
+                y1 = this.offset_m[s_pos, w_pos];
+                y2 = this.offset_m[s_pos, w_pos + 1];
+                y3 = this.offset_m[s_pos, w_pos + 2];
+            }
+            else
+            {
+                throw new Exception("axis가 범위를 벗어났습니다.");
+            }
+
+            return x * (y1 + 4 * y2 + y3) / 3.0;
+        }
+
+    }
+
+}
---
+++ LiveClinic05Example/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace LiveClinic05Example
+{
+    class Program
+    {
+        /*static int[,] offsetTable1_retire = new int[,] {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1063},
+            {0, 2406, 3206, 3565, 3678, 3619, 3402, 3131, 2968, 3001, 3252, 3827, 4814, 6329, 8317, 10380, 12274},
+            {1980, 6807, 8190, 9059, 9678, 10145, 10541, 10954, 11466, 12154, 13093, 14276, 15597, 16949, 18245, 19424, 20493},
+            {6384, 11580, 13364, 14648, 15724, 16688, 17598, 18500, 19402, 20311, 21228, 22144, 23039, 23885, 24666, 25372, 26001},
+            {11520, 16453, 18528, 20045, 21296, 22410, 23415, 24310, 25106, 25826, 26471, 27039, 27541, 27979, 28357, 28679, 28953},
+            {16608, 21227, 23138, 24564, 25705, 26633, 27391, 28007, 28502, 28896, 29207, 29450, 29636, 29775, 29875, 29944, 29984},
+            {21264, 25255, 26779, 27784, 28512, 29058, 29454, 29723, 29891, 29978, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {24816, 27985, 28981, 29547, 29861, 29990, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {26736, 29221, 29818, 29997, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {27400, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {27400, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {27400, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {27400, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {27400, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {27400, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {27400, 29449, 29930, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {25788, 28535, 29419, 29851, 29975, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000, 30000},
+            {21972, 26146, 27478, 28336, 28935, 29340, 29586, 29706, 29754, 29760, 29760, 29760, 29760, 29760, 29760, 29760, 29760},
+            {15192, 21267, 23221, 24483, 25393, 26105, 26632, 26983, 27227, 27368, 27389, 27362, 27359, 27361, 27359, 27360, 27360},
+            {5880, 12220, 14539, 16152, 17372, 18277, 19013, 19626, 20082, 20421, 20667, 20831, 20916, 20934, 20899, 20832, 20772},
+            {0, 1875, 3102, 4014, 4751, 5371, 5883, 6295, 6615, 6850, 7005, 7067, 7023, 6868, 6600, 6190, 5568},
+        };*/
+
+        static double station_interval = 16.0;
+        static double waterline_interval = 1.0;
+
+
+
+        static int[,] ReadOffsetData(string filename)
+        {
+            int[,] offset = null;
+
+            using (FileStream fs = new FileStream(filename, FileMode.Open))
+            using (StreamReader sr = new StreamReader(fs))
+            {
+                List<int[]> offset_list = new List<int[]>();
+
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    string[] str_vals = line.Split(',');
+                    int[] vals = new int[str_vals.Length];
+
+                    for (int i = 0; i < str_vals.Length; i++)
+                    {
+                        vals[i] = Convert.ToInt32(str_vals[i]);
+                    }
+
+                    offset_list.Add(vals);
+                }
+
+                // for return as int[,] type
+                offset = new int[offset_list.Count, offset_list[0].Length];
+
+                for (int i = 0; i < offset_list.Count; i++)
+                {
+                    for (int j = 0; j < offset_list[0].Length; j++)
+                    {
+                        offset[i, j] = offset_list[i][j];
+                    }
+                }
+            }
+
+            return offset;
+        }
+
+
+        static void WriteOffsetData(string filename, int[,] data)
+        {
+            using (FileStream fs = new FileStream(filename, FileMode.Create))
+            using (StreamWriter sw = new StreamWriter(fs))
+            {
+                for (int i = 0; i < data.GetLength(0); i++)
+                {
+                    string line = "";
+
+                    for (int j = 0; j < data.GetLength(1); j++)
+                    {
+                        line += ", " + data[i, j].ToString();
+                    }
+
+                    line = line.Trim(',', ' ');
+                    sw.WriteLine(line);
+                }
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            // Write .csv file
+            // WriteOffsetData("offset_output.csv", offsetTable1);
+
+            // Read .csv file, get offset data
+            int[,] offset_data = Program.ReadOffsetData("offset_input.csv");
+
+
+            HydrostaticCalculator calc = new HydrostaticCalculator_Simpson1(offset_data, station_interval, waterline_interval);
+            double[] areas = calc.Calculate_waterline_areas();
+
+            Console.WriteLine(areas.Length);
+            foreach (double area in areas)
+            {
+                Console.WriteLine(area);
+            }
+
+        }
+    }
+}
